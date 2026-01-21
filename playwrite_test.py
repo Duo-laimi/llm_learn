3a042942146af5d998fab87768ca960108fc9e55
supeng <supengscu@qq.com>
2026-01-21 13:58:42 +0800

    langchain: 自定义工具，工具链组合

--- playwrite_test.py
+++ playwrite_test.py
@@ -1,31 +1,44 @@
 from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
 from langchain_community.tools.playwright.utils import create_sync_playwright_browser
 from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor, create_openai_tools_agent, create_openai_functions_agent
+from langchain_classic.agents import AgentExecutor, create_openai_tools_agent
 from langchain.chat_models import init_chat_model
-import os
+from langchain.tools import tool
 from dotenv import load_dotenv
 load_dotenv()
 
-DeepSeek_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+@tool
+def summarize_website(url: str) -> str:
+    """访问指定网站并返回内容总结"""
+    try:
+        sync_browser = create_sync_playwright_browser()
+        toolkit = PlayWrightBrowserToolkit.from_browser(sync_browser=sync_browser)
+        tools = toolkit.get_tools()
+        prompt = hub.pull("hwchase17/openai-tools-agent")
+        model = init_chat_model("deepseek-chat", model_provider="deepseek")
+        agent = create_openai_tools_agent(model, tools, prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-sync_browser = create_sync_playwright_browser()
-toolkit = PlayWrightBrowserToolkit.from_browser(sync_browser=sync_browser)
-tools = toolkit.get_tools()
+        command = {
+        "input": f"访问这个网站 {url} 并详细总结一下这个网站的内容。"
+        }
+        response = agent_executor.invoke(command)
+        return response.get("output", "无法获取网站内容总结。")
+    except Exception as e:
+        return f"网站访问失败：{e}"
 
-prompt = hub.pull("hwchase17/openai-tools-agent")
-print(prompt)
+@tool
+def generate_pdf(text: str) -> str:
+    """将输入内容转换为pdf保存在本地"""
+    return "PDF文件已成功生成"
 
-model = init_chat_model("deepseek-chat", model_provider="deepseek")
+simple_chain = summarize_website | generate_pdf
 
-agent = create_openai_tools_agent(model, tools, prompt)
+from langchain_classic.tools import Tool
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-command = {
-    "input": "访问这个网站https://www.zhihu.com/hot，简要介绍前3条热点信息。"
-}
-
-response = agent_executor.invoke(command)
-
-print(response)
+# 输入和输出都是str
+simple_tool = Tool.from_function(
+    func=simple_chain.invoke,
+    description="访问指定网站，并进行总结，将结果保存为pdf文件。",
+    name="summary_and_save"
+)
